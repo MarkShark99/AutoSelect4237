8467148b29f1714366e6ea1fe8818e3c2ef58dc3
Mark Washington <markdw88@gmail.com>
2019-01-06 11:30:45 -0500

    Edited variable names in 2019 frame

--- lib/frames/GameFrame2019.py
+++ lib/frames/GameFrame2019.py
@@ -3,15 +3,15 @@ import lib.widgets.RobotPosition
 
 class GameFrame2019(QtWidgets.QFrame):
     def __init__(self):
-        super(GameFrame2019, self).__init__();
+        super(GameFrame2019, self).__init__()
         self.iconSize = 150
 
         '''
         Widgets
         '''
 
-        self.robotPixmap2018 = QtGui.QPixmap("img/2018/icons/RobotIcon.png")
-        self.blankPixmap2018 = QtGui.QPixmap("img/2018/icons/TransparentRobotIcon.png")
+        self.robotPixmap2019 = QtGui.QPixmap("img/2018/icons/RobotIcon.png")
+        self.blankPixmap2019 = QtGui.QPixmap("img/2018/icons/TransparentRobotIcon.png")
 
         self.layout = QtWidgets.QGridLayout()
 
